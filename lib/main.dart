36847bd002de77e77d21fb918de1ebfc9d5714eb
srivani1224 <srivanireddy1432@gmai.com>
2022-11-12 10:43:02 +0530

    home updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'vnr connect',
-      home: Scaffold(
-        body: Center(
-            child: Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(250),
-          child: Column(
-            children: <Widget>[
-              const Text(
-                "VNR CONNECT",
-                textAlign: TextAlign.center,
+      home: My_App(),
+    );
+  }
+}
+
+class My_App extends StatelessWidget {
+  const My_App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(250),
+        child: Column(
+          children: <Widget>[
+            const Text(
+              "VNR CONNECT",
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.lightBlueAccent,
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                  );
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.lightBlueAccent,
-                ),
-                child: const Text("Explore"),
-              )
-            ],
-          ),
-        )),
-      ),
+              child: const Text("Explore"),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   HomeState createState() => HomeState();
 }
@@ -9,11 +11,20 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: const [
-        Text("VNR CONNECT"),
-        TextButton(onPressed: null, child: Text("Register")),
-        TextButton(onPressed: null, child: Text("Login"))
-      ]),
+      appBar: AppBar(
+          //backgroundColor: Colors.lightBlueAccent,
+          title: const Text('VNR CONNECT'),
+          centerTitle: true,
+          actions: [
+            TextButton(
+                onPressed: null,
+                style: TextButton.styleFrom(foregroundColor: Colors.black),
+                child: const Text("Register")),
+            TextButton(
+                onPressed: null,
+                style: TextButton.styleFrom(foregroundColor: Colors.purple),
+                child: const Text("Login")),
+          ]),
     );
   }
 }
