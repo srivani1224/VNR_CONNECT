79691f2e648ccc6717a9d1388d9dfc32fea2c8cd
srivani1224 <srivanireddy1432@gmai.com>
2022-11-12 20:07:13 +0530

    registration and login intro screens added

--- lib/screens/login_intro.dart
+++ lib/screens/login_intro.dart
@@ -1,24 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:vnr_connect/screens/login_intro.dart';
 import 'package:vnr_connect/screens/registration_intro.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class LoginIntro extends StatefulWidget {
+  const LoginIntro({super.key});
 
   @override
-  HomeState createState() => HomeState();
+  LoginIntroState createState() => LoginIntroState();
 }
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-];
-
-class HomeState extends State<Home> {
+class LoginIntroState extends State<LoginIntro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,55 +49,26 @@ class HomeState extends State<Home> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                CarouselSlider(
-                  items: imgList
-                      .map((item) => Container(
-                            child: Center(
-                              child: Image.network(
-                                item,
-                                fit: BoxFit.cover,
-                                width: 1000,
-                              ),
-                            ),
-                          ))
-                      .toList(),
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    aspectRatio: 2.0,
-                    enlargeCenterPage: true,
-                  ),
-                ),
                 InkWell(
                   onTap: () {},
-                  child: Card(
+                  child: const Card(
                       color: Colors.lightBlueAccent,
                       margin: EdgeInsets.all(20),
                       child: Padding(
                         padding:
                             EdgeInsets.symmetric(horizontal: 100, vertical: 40),
-                        child: Text("Clutural clubs"),
+                        child: Text("Login as Organizer/Head"),
                       )),
                 ),
                 InkWell(
                   onTap: () {},
-                  child: Card(
+                  child: const Card(
                       color: Colors.lightBlueAccent,
                       margin: EdgeInsets.all(20),
                       child: Padding(
                         padding:
                             EdgeInsets.symmetric(horizontal: 90, vertical: 40),
-                        child: Text("Student Chapters"),
-                      )),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Card(
-                      color: Colors.lightBlueAccent,
-                      margin: EdgeInsets.all(20),
-                      child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 127, vertical: 40),
-                        child: Text("Fests"),
+                        child: Text("Login as VNR Student/Faculty"),
                       )),
                 ),
               ],
