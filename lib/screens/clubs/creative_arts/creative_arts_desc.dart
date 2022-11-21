9b211b3ec7bf1d9c4ee5bf486d9b60f833413382
srivani1224 <srivanireddy1432@gmai.com>
2022-11-21 12:31:22 +0530

    connected to firebase

--- lib/screens/clubs/creative_arts/creative_arts_desc.dart
+++ lib/screens/clubs/creative_arts/creative_arts_desc.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:vnr_connect/screens/clubs/creative_arts/creative_arts_options_display.dart';
+import 'package:vnr_connect/screens/clubs/creative_arts/creative_arts_events.dart';
+import 'package:vnr_connect/screens/clubs/creative_arts/creative_arts_heads.dart';
+import 'package:vnr_connect/screens/clubs/creative_arts/creative_arts_joining_requests.dart';
+import 'package:vnr_connect/screens/clubs/creative_arts/creative_arts_members.dart';
 
 class CreativeArtsDesc extends StatefulWidget {
   const CreativeArtsDesc({super.key});
@@ -18,21 +21,6 @@ class CreativeArtsDescState extends State<CreativeArtsDesc> {
           backgroundColor: Colors.lightBlueAccent,
           title: const Text('VNR CONNECT'),
           centerTitle: true,
-          leading: TextButton.icon(
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-            ),
-            icon: Icon(Icons.menu),
-            label: Text(""),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const CreativeArtsOptionsDisplay()),
-              );
-            },
-          ),
           actions: [
             TextButton.icon(
               style: TextButton.styleFrom(
@@ -51,6 +39,73 @@ class CreativeArtsDescState extends State<CreativeArtsDesc> {
               onPressed: () {},
             ),
           ]),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Events'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreativeArtsEvents()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Heads'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreativeArtsHeads()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Members'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreativeArtsMembers()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Joining Requests'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const CreativeArtsJoiningRequests()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 200, vertical: 75),
         width: 900,
